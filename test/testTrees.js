7e81682d358b90d9b3a04cb4b91c26b1aa65da36
rotsee <mejl@leowallentin.se>
2019-09-01 00:18:24 +0200

    more tests

--- test/testTrees.js
+++ test/testTrees.js
@@ -7,8 +7,31 @@ describe('Calling trees', function() {
   it('should return a long list of trees', function(done) {
     request
       .get('/trees')
-      .expect(200, done)
+      .expect(200)
       .expect("Content-Type", "application/json; charset=utf-8")
-      .expect(x => x.length > 1000)
+      .then(r => {
+        assert(r.body.length > 1000)
+        assert(r.body.every(x => x.key))
+        done()
+      })
   })
+})
+
+describe('Using a bbox', function() {
+  it('should return a limited list of trees', function(done) {
+    request
+      .get('/trees?bbox=59.1,10,59.2,20')
+      .expect(200)
+      .expect("Content-Type", "application/json; charset=utf-8")
+      .then(r => {
+        assert(r.body.length)
+        assert(r.body.length < 1000)
+        done()
+      })
+    })
+  it('should return an error on invalid coordinates', function(done) {
+    request
+      .get('/trees?bbox=59.1,10,59.2')
+      .expect(409, done)
+    })
 })

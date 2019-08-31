4eacc2d776c9ea4597ddc1e53433532c29f6d079
rotsee <mejl@leowallentin.se>
2019-08-31 17:49:58 +0200

    make delete work

--- routes/v1/delete-tree.js
+++ routes/v1/delete-tree.js
@@ -10,8 +10,10 @@ let endpoint = (req, res, next) => {
   })
 
   client.connect()
-  let query = "UPDATE trees SET deleted_at = $1, deleted_by = $2 WHERE ssm_key=$1;"
-  client.query(query, [req.params.key], (err) => {
+  const user_ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress
+  const time = new Date()
+  const query = "UPDATE trees SET deleted_at = $2, deleted_by = $3 WHERE ssm_key=$1;"
+  client.query(query, [req.params.key, time, user_ip], (err) => {
     if (err) {
       return next(new InternalServerError(`Error connecting to database: ${err}`))
     }

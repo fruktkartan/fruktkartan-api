75ba975cd4b1ee977692451ae29d17aec7d3619d
rotsee <mejl@leowallentin.se>
2020-02-18 20:23:01 +0100

    add creation dates for tree

--- routes/v1/tree.js
+++ routes/v1/tree.js
@@ -9,7 +9,7 @@ let endpoint = (req, res, next) => {
 
   client.connect()
   let query =
-    "SELECT description, img, img_200, img_400, img_800, type FROM trees WHERE ssm_key=$1;"
+    "SELECT description, added_at, img, img_200, img_400, img_800, type FROM trees WHERE ssm_key=$1;"
   client.query(query, [req.params.key], (err, data) => {
     if (err) {
       return next(
@@ -24,6 +24,7 @@ let endpoint = (req, res, next) => {
         : null,
       description: tree.description,
       type: tree.type.trim(),
+      added: tree.added_at,
     })
     return next()
   })

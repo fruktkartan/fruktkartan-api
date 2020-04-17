afeeeb911b3450a4cd90afa0ac7dae98fd435585
rotsee <mejl@leowallentin.se>
2020-04-17 10:14:35 +0200

    linting

--- routes/v1/sign.js
+++ routes/v1/sign.js
@@ -6,7 +6,8 @@ const murmurhash = require("murmurhash")
 
 module.exports = (req, res, next) => {
   const fileName = req.query["file-name"]
-  const newFileName = murmurhash.v3(fileName, Date.now()) + fileName.split(".").pop()
+  const newFileName =
+    murmurhash.v3(fileName, Date.now()) + fileName.split(".").pop()
 
   getSignedRequest(newFileName, process.env.S3_BUCKET, process.env.S3_REGION)
     .then(data => res.json(data))

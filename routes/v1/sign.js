1f4efe323865e0adc56607024fe7ebbcac2f5909
rotsee <mejl@leowallentin.se>
2020-04-18 21:25:11 +0200

    include dot in file name

--- routes/v1/sign.js
+++ routes/v1/sign.js
@@ -5,9 +5,9 @@ const { getSignedRequest } = require("./s3")
 const murmurhash = require("murmurhash")
 
 module.exports = (req, res, next) => {
-  const fileName = req.query["file-name"]
+  const fileName = req.params["file-name"]
   const newFileName =
-    murmurhash.v3(fileName, Date.now()) + fileName.split(".").pop()
+    murmurhash.v3(fileName, Date.now()) + "." + fileName.split(".").pop()
 
   getSignedRequest(newFileName, process.env.S3_BUCKET, process.env.S3_REGION)
     .then(data => res.json(data))

c586648e83291f8b818047b3894337d1d1e8283d
rotsee <mejl@leowallentin.se>
2020-05-21 12:21:53 +0200

    allow extreme bbox coords

--- routes/v1/utils.js
+++ routes/v1/utils.js
@@ -4,10 +4,10 @@ utils.isValidCoords = (lat, lon) => {
   return (
     parseFloat(lat) &&
     parseFloat(lon) &&
-    parseFloat(lat) > -90 &&
-    parseFloat(lat) < 90 &&
-    parseFloat(lon) > -180 &&
-    parseFloat(lon) < 180
+    parseFloat(lat) >= -90 &&
+    parseFloat(lat) <= 90 &&
+    parseFloat(lon) >= -180 &&
+    parseFloat(lon) <= 180
   )
 }
 

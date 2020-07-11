c799c25ca4e9926a1dcfb248072c38cc23c70457
rotsee <mejl@leowallentin.se>
2020-07-11 13:36:38 +0200

    test for GET tree

--- test/testTree.js
+++ test/testTree.js
@@ -10,4 +10,13 @@ describe("Deleting a tree", function () {
   })
 })
 
+describe("Asking for a tree", function () {
+  it("should return an error for an invalid request", function (done) {
+    request(app).get("/tree/").expect(409, done)
+  })
+  it("should return an error if the tree doesn't exist", function (done) {
+    request(app).get("/tree/this-is-not-a-valid-tree-id").expect(404, done)
+  })
+})
+
 app.close()
